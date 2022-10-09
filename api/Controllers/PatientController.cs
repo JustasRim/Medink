52f941feac2f8100a3f370a08e0c46757543354a
Justas Rimkus <jusrim3@ktu.lt>
2022-10-09 14:57:25 +0300

    Added patient controller

--- api/Controllers/PatientController.cs
+++ api/Controllers/PatientController.cs
@@ -8,41 +8,41 @@ namespace api.Controllers
     [ApiController]
     public class PatientController : Controller
     {
-        private IMedicService _medicService;
+        private IPatientService _patientService;
 
-        public PatientController(IMedicService medicService)
+        public PatientController(IPatientService patientService)
         {
-            _medicService = medicService;
+            _patientService = patientService;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return Ok(await _medicService.Get());
+            return Ok(await _patientService.Get());
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
-            return Ok(await _medicService.Get(id));
+            return Ok(await _patientService.Get(id));
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Medic medic)
+        public async Task<IActionResult> Create([FromBody] Patient patient)
         {
-            return Ok(await _medicService.Create(medic));
+            return Ok(await _patientService.Create(patient));
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update([FromBody] Medic medic)
+        public async Task<IActionResult> Update([FromBody] Patient patient)
         {
-            return Ok(await _medicService.Update(medic));
+            return Ok(await _patientService.Update(patient));
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            return Ok(await _medicService.Delete(id));
+            return Ok(await _patientService.Delete(id));
         }
     }
 }
