d5de606c0f213b9655e8c1167b26fc11e491e62c
Justas Rimkus <jusrim3@ktu.lt>
2022-11-06 22:08:22 +0200

    hamburger menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
 import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <MainLayout>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<Registration />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./layout.scss";
+
 type Props = {
   children: React.ReactNode;
 };
 
 const MainLayout = ({ children }: Props) => {
-  return <div className="App">{children}</div>;
+  const [expanding, setExpanding] = useState(true);
+
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="header__links">
+          <Link className="header__link" to="/">
+            Home
+          </Link>
+          <Link className="header__link" to="/sign-up">
+            Register
+          </Link>
+          <Link className="header__link" to="/login">
+            Login
+          </Link>
+        </div>
+        <div
+          className={`hamburger${expanding ? "" : " hamburger--expanding"}`}
+          onClick={() => setExpanding((curr) => !curr)}
+          aria-label="open the menu"
+        >
+          <span className="hamburger__top"></span>
+          <span className="hamburger__middle"></span>
+          <span className="hamburger__bottom"></span>
+        </div>
+      </header>
+      {children}
+      <footer></footer>
+    </div>
+  );
 };
 
 export default MainLayout;
