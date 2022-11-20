ddacd7e1efaf6405badb839238b4cf004e3610fb
Justas Rimkus <jusrim3@ktu.lt>
2022-11-20 17:34:54 +0200

    moved some files again

--- Tests/Application.IntegrationTests/AuthenticationControllerTests.cs
+++ Tests/Application.IntegrationTests/AuthenticationControllerTests.cs
@@ -1,10 +1,24 @@
+using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Application.IntegrationTests
 {
     public class AuthenticationControllerTests
     {
+        private ApplicationDbContext context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+               .UseInMemoryDatabase(Guid.NewGuid().ToString())
+               .Options);
+
+        //private Medic medic = new Medic()
+        //{
+        //    Id = 1,
+        //    Name = "Name",
+        //    LastName = "Surname",
+        //    Email = "test@email.com"
+        //};
+
         [Fact]
         public void When_LoginIn_Expect_JWTToken()
         {
