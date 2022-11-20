6e44280af06ca6d299351201c800c263aa6c7b2f
gytisgedm <gytis.gedmantas@gmail.com>
2022-11-20 19:19:52 +0200

    Added nested get unit tests

--- Tests/Application.Tests/Services/MedicServiceTests.cs
+++ Tests/Application.Tests/Services/MedicServiceTests.cs
@@ -17,7 +17,8 @@ namespace Application.Tests.Services
             Id = 1,
             Name = "Name",
             LastName = "Surname",
-            Email = "test@email.com"
+            Email = "test@email.com",
+            Patients = new List<Patient>()
         };
         private Medic medic2 = new Medic()
         {
@@ -25,6 +26,32 @@ namespace Application.Tests.Services
             Name = "Name2",
             LastName = "Surname2",
             Email = "test2@email.com"
+        };
+        private Patient patient = new Patient()
+        {
+            Id = 1,
+            Name = "Name",
+            LastName = "Surname",
+            Email = "test@email.com",
+            MedicId = 1,
+            Medic = new Medic(),
+            Symptoms = new List<Symptom>()
+        };
+        private Symptom symptom1 = new Symptom()
+        {
+            Id = 1,
+            Name = "Name",
+            Description = "Description",
+            Patient = new Patient(),
+            PatientId = 1
+        };
+        private Symptom symptom2 = new Symptom()
+        {
+            Id = 2,
+            Name = "Name2",
+            Description = "Description2",
+            Patient = new Patient(),
+            PatientId = 1,
         };
 
         [Fact]
@@ -92,6 +119,35 @@ namespace Application.Tests.Services
 
             var result = await medicService.Update(medic);
             Assert.True(result == null);
+        }
+
+        [Fact]
+        public async void ShouldReturnEmptySymptomsList()
+        {
+            MedicService medicService = new MedicService(context);
+
+            var result = medicService.GetSymptoms(1, 1);
+            Assert.True(result.Count == 0);
+        }
+
+        [Fact]
+        public async void ShouldGetPatientSymptoms()
+        {
+            MedicService medicService = new MedicService(context);
+            medic.Patients.Add(patient);
+            patient.Medic = medic;
+            patient.Symptoms.Add(symptom1);
+            patient.Symptoms.Add(symptom2);
+            symptom1.Patient = patient;
+            symptom2.Patient = patient;
+            context.Medics.Add(medic);
+            context.Patients.Add(patient);
+            context.Symptoms.Add(symptom1);
+            context.Symptoms.Add(symptom2);
+
+            var symptoms = medicService.GetSymptoms(1, 1);
+            Assert.True(symptoms.Contains(symptom1));
+            Assert.True(symptoms.Contains(symptom2));
         }
     }
 }
